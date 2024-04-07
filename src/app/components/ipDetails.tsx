d2ef876945333de0832aac3750be526852a6c9ab
Subramonian Inian <risubramonian@gmail.com>
2024-04-07 20:15:18 +0530

    Code cleanup

--- src/app/components/ipDetails.tsx
+++ src/app/components/ipDetails.tsx
@@ -1,12 +1,8 @@
-
-// import { useState, useEffect } from 'react';
 import { publicIpv4, publicIpv6 } from 'public-ip';
-import { IoCopyOutline } from "react-icons/io5";
 import { TbInfoCircle } from "react-icons/tb";
 import Copy from './copy';
 
 async function IpDetails () {
-//   const [ips, setIps] = useState([{ version: '', address: '' }]);
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
     const fetchData = async () => {
 
@@ -23,7 +19,7 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
     var ips = await fetchData();
 
       if (!ips) {
-    return <p className='mt-10 text-center'>Sorry, no posts available.</p>;
+    return <p className='mt-10 text-center'>Sorry, unable to fetch your IP address.</p>;
   }
 
     return( 
@@ -33,7 +29,6 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
             <div key={index} className=" left-0 top-0 flex justify-between w-auto border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit static rounded-xl border lg:bg-gray-200 gap-10 items-center">
               <div className='bg-green-600 p-4 rounded-l-xl'>{ip.version}</div>
               <code className="font-mono font-bold text-left flex-1 w-96">{ip.address} </code>
-              {/* <IoCopyOutline size='1.25em' className='green-500 cursor-pointer text-green-600' /> */}
               <Copy copyString={ip.address} isloading={false}/>
             </div>
             <div className='flex items-center gap-3 mt-2 justify-end'>
