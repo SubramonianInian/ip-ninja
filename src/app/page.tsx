06f59f37aa8922eb90a9a0beb744d32ae04c72e9
Subramonian Inian <risubramonian@gmail.com>
2024-04-07 20:09:59 +0530

    Updating faviicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
               <div className='w-auto gap flex flex-col justify-center gap-4'>
 
             <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Want to know your </span> IP address?</h1>
-            <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">An IP address is like a digital street address for devices connected to the internet. It helps data find its way to the right destination, whether it's a computer, phone, or any other device online.</p>
+            <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">An IP address is like a digital street address for devices connected to the internet. It helps data find its way to the right destination, whether it&apos;s a computer, phone, or any other device online.</p>
       <IpDetails />
       </div>
     </main>
